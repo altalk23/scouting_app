6b4cec5cb9a3329aaf2408fa57126f748ca92443
altalk.23 <altalk.23@robcol.k12.tr>
2019-08-24 12:37:36 +0300

    Split up dart file, part 3 Lay out custom label

--- lib/widget/custom_button.dart
+++ lib/widget/custom_button.dart
@@ -4,11 +4,11 @@ import 'package:flutter/foundation.dart';
 
 class CustomButton extends StatelessWidget {
     CustomButton({@required this.onPressed, this.child, this.color});
-    
+
     final VoidCallback onPressed;
     final Widget child;
     final Color color;
-    
+
     @override
     Widget build(BuildContext context) {
         // TODO: implement button
--- lib/widget/custom_label.dart
+++ lib/widget/custom_label.dart
@@ -1,1 +1,18 @@
 import 'dart:core';
+import 'package:flutter/material.dart';
+
+class CustomLabel extends StatelessWidget {
+    CustomLabel(this.data, {this.style});
+    
+    final String data;
+    final TextStyle style;
+    
+    @override
+    Widget build(BuildContext context) {
+        // TODO: implement label
+        return Text(
+            data,
+            style: style,
+        );
+    }
+}
