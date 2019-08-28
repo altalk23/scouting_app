efef0e23c95aab51e87d80c26b2e6dbd133fcfa9
altalk.23 <altalk.23@robcol.k12.tr>
2019-08-28 23:21:09 +0300

    Split up dart file, part 18 Copy the qr screen

--- lib/screen/qr_screen.dart
+++ lib/screen/qr_screen.dart
@@ -1,1 +1,56 @@
-import 'dart:core';
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:scouting_app/widget/custom_button.dart';
+import 'package:scouting_app/widget/custom_label.dart';
+
+class QRScreen extends StatefulWidget {
+    @override
+    _QRScreen createState() => _QRScreen();
+}
+
+class _QRScreen extends State<QRScreen> {
+    String qrData = "";
+    int _index;
+    
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: CustomLabel("QR List"),
+                actions: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.remove),
+                        onPressed: () => history.removeAt(_index),
+                    )
+                ],
+            ),
+            body: ListView.builder(
+                itemCount: history.length,
+                itemBuilder: (context, index) {
+                    return index != 0 ?
+                    new CustomButton(
+                        child: CustomLabel(
+                            "Team " + history[index - 1].split(",")[0],
+                            style: TextStyle(
+                                fontSize: 28.0,
+                            ),
+                        ),
+                        onPressed: () {
+                            setState(() {
+                                _index = index - 1;
+                                qrData = history[index - 1];
+                            });
+                        },
+                    ) :
+                    QrImage(
+                        version: 10,
+                        data: qrData,
+                        size: MediaQuery.of(context).size.width,
+                    );
+                },
+            ),
+        );
+    }
+    
+}
+
